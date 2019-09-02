d733a81b768f99ec67dd92527a5ba0b7fa796e28
CarlosVitorRibeiro <38073255+CarlosVitorRibeiro@users.noreply.github.com>
2019-09-02 20:15:29 -0300

    Update Atividade1.java

--- Atividade1.java
+++ Atividade1.java
@@ -1,20 +1,20 @@
+
 package atividade1;
 
 
 public class Atividade1 {
-    
-               
-    
+
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-     int cateto1 = 10;
-     int cateto2 = 20;
-     
-         double hipotenusa = teoremaPitagoras(cateto1, cateto2);
-       
+        int cateto1 = 10;
+        int cateto2 = 20;
+        double hipotenusa = teoremaPitagoras(cateto1,cateto2);
     }
-   private static double teoremaPitagoras(int x, int y){
-       return Math.sqrt(x)+ (y);
-   }
-    
-    
+
+    private static double teoremaPitagoras(int cate1, int cate2) {
+        return Math.sqrt((cate1 * cate1) + (cate2 * cate2));
+    }
+
 }
