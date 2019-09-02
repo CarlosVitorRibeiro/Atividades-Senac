98479e2d913cb565527f4cd0687702818d806be5
CarlosVitorRibeiro <38073255+CarlosVitorRibeiro@users.noreply.github.com>
2019-09-02 19:56:55 -0300

    first commit

---
+++ Atividade1.java
@@ -0,0 +1,15 @@
+
+package atividade1;
+
+
+public class Atividade1 {
+
+    
+    public static void main(String[] args) {
+        int cateto1 = 10;
+        int cateto2 = 20;
+               
+    }
+    
+    
+}
