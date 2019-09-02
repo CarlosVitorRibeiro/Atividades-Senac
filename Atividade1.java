b967854cb0c4db52d117090d2878d72d7582ca7b
CarlosVitorRibeiro <38073255+CarlosVitorRibeiro@users.noreply.github.com>
2019-09-02 20:01:32 -0300

    Update Atividade1.java

--- Atividade1.java
+++ Atividade1.java
@@ -1,15 +1,20 @@
-
 package atividade1;
 
 
 public class Atividade1 {
-
+    
+               
     
     public static void main(String[] args) {
-        int cateto1 = 10;
-        int cateto2 = 20;
-               
+     int cateto1 = 10;
+     int cateto2 = 20;
+     
+         double hipotenusa = teoremaPitagoras(cateto1, cateto2);
+       
     }
+   private static double teoremaPitagoras(int x, int y){
+       return Math.sqrt(x)+ (y);
+   }
     
     
 }
